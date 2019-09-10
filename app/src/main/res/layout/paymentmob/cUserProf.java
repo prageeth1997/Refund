f61c71a7290ad0eae9227d5a320bf4210f496d1e
ChamalHerath <chamaljanadara@gmail.com>
2019-09-10 15:09:34 +0530

    Second commit by chamal

---
+++ app/src/main/res/layout/paymentmob/cUserProf.java
@@ -0,0 +1,40 @@
+package com.example.paymentmob;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class cUserProf extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cactivity_user_prof);
+
+        button = (Button) findViewById(R.id.btndelete);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(cUserProf.this);
+
+                builder.setMessage("Are you sure ?").setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                }).setNegativeButton("No", null);
+
+
+                AlertDialog alert = builder.create();
+                alert.show();
+            }
+        });
+    }
+
+
+}
